7be722db0b620612ab00843c2e5194b91de8d730
Alex <brasero_alexandre@hotmail.com>
2018-04-06 14:31:59 -0400

    Added 2 characters

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+#region Variables (public)
+
+	public bool m_bLockYRotation = false;
+	public float m_fMaxXRotation = 0.0f;
+
+	#endregion
+
+#region Variables (private)
+
+    static Camera s_pCamera = null;
+
+    #endregion
+
+
+    private void Awake()
+    {
+        if (s_pCamera == null)
+            s_pCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+	{
+		Vector3 tSpriteForward = transform.position - s_pCamera.transform.position;
+
+		float fPreviousRotationZ = transform.localEulerAngles.z;
+		transform.forward = tSpriteForward.normalized;
+
+		Vector3 tNewEuleur = transform.localEulerAngles;
+		if (m_fMaxXRotation != 0.0f)
+			tNewEuleur.x = Mathf.Clamp(tNewEuleur.x, 0.0f, m_fMaxXRotation);
+		tNewEuleur.z = fPreviousRotationZ;
+		transform.localEulerAngles = tNewEuleur;
+
+		if (m_bLockYRotation)
+		{
+			Vector3 tWorldRotation = transform.eulerAngles;
+			tWorldRotation.y = s_pCamera.transform.localEulerAngles.y;
+			transform.eulerAngles = tWorldRotation;
+		}
+	}
+}
---
+++ Assets/Shaders/LitSprite.shader
@@ -0,0 +1,49 @@
+﻿Shader "Custom/LitSprite"	// To enable shadowcasting on sprite renderers, set inspector mode to "debug"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1, 1, 1, 1)
+		[PerRendererData] _MainTex("Sprite Texture", 2D) = "white" {}
+		_AlphaCutout("Alpha Cutout", Range(0, 1)) = 0.5
+	}
+
+	SubShader
+	{
+		Tags
+		{
+			"Queue" = "Geometry"
+			"RenderType" = "TransparentCutout"
+		}
+
+		LOD 200
+		Cull Off
+
+		CGPROGRAM
+
+		#pragma surface Surf Lambert addshadow fullforwardshadows
+		#pragma target 5.0
+
+
+		sampler2D _MainTex;
+		fixed4 _Color;
+		half _AlphaCutout;
+
+		struct Input
+		{
+			half2 uv_MainTex	: TEXCOORD0;
+			half4 vertexColor	: COLOR;
+		};
+
+		void Surf(Input tIn, inout SurfaceOutput tOut)
+		{
+			fixed4 tColor = tex2D(_MainTex, tIn.uv_MainTex) * (tIn.vertexColor * _Color);
+			tOut.Albedo = tColor.rgb;
+			tOut.Alpha = tColor.a;
+			clip(tOut.Alpha - _AlphaCutout);
+		}
+
+		ENDCG
+	}
+
+	FallBack "Sprites/Default"
+}
